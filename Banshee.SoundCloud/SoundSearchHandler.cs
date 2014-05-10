7238ad3308c20ea37111ac98cc4cb68daa6f93c9
mhewedy <mohammed_a_hewedy@hotmail.com>
2014-05-10 18:24:41 +0300

    change the code so that you can add Any ActionEntries laster easy

--- Banshee.SoundCloud/SoundSearchHandler.cs
+++ Banshee.SoundCloud/SoundSearchHandler.cs
@@ -1,11 +1,18 @@
 using System;
 
-namespace Foo
+using Gtk;
+using Banshee.Sources;
+
+namespace Banshee.SoundCloud
 {
-	public class SoundSearchHandler
+	public class SoundSearchHandler : IToolbarButtonHandler
 	{
-		public SoundSearchHandler()
-		{
+		public SoundSearchHandler(PrimarySource primarySource) : base(primarySource){}
+
+		public void toolBarButtonClicked(object o, EventArgs args){
+			BaseDialog editor = new BaseDialog("Search Sounds", "Type part of sound name to search", "Sound contains", Stock.Find);
+			//editor.Response += OnArtistAdditionResponse;
+			editor.Show();
 		}
 	}
 }
